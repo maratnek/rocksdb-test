aa15cf8300f3ce844e1a458b7cb971afa6cdb474
Zabirov Marat Ilsurovich <92zabirovmi@region.cbr.ru>
2021-09-09 17:25:56 +0300

    improve multi test

--- multi-rocks-load.cpp
+++ multi-rocks-load.cpp
@@ -20,7 +20,7 @@ std::string kDBPath = "C:\\Windows\\TEMP\\rocksdb_simple_example";
 #else
 std::string kDBPath = "./rocksdb_simple_example";
 #endif
-
+std::atomic<uint32_t> atom_count = 0;
 std::mutex mutex_gener_add_block;
 
 int main() {
@@ -36,8 +36,11 @@ int main() {
     Status s = DB::Open(options, kDBPath, &db);
     assert(s.ok());
 
-    auto count = 100'000'000;
-    std::unordered_map<std::string, std::string> v_r_str(count);
+    auto count = 1000'000;
+//    std::unordered_map<std::string, std::string> v_r_str;
+    std::vector<std::string> v_r_str;
+    v_r_str.reserve(count);
+    v_r_str.resize(count);
     std::cout << "Prepare data " << std::endl;
 
     auto l_prepare_data = [&]() {
@@ -56,8 +59,9 @@ int main() {
             std::cout << "Iter threads: " << ith << " Interval start: " << i << " Interval end: " << max_count << std::endl;
             for (; i < max_count; i++)
             {
-                std::lock_guard<std::mutex> lock(mutex_gener_add_block);
-                v_r_str.insert({std::to_string(i),gen_random_str(20)});
+//                std::lock_guard<std::mutex> lock(mutex_gener_add_block);
+//                v_r_str.insert({std::to_string(i),gen_random_str(20)});
+                v_r_str[i] = gen_random_str(20);
             }
         })));
     }
@@ -70,27 +74,113 @@ int main() {
     };
 
     auto start = std::chrono::steady_clock::now();
-    for (int i = 0; i < count; ++i) {
-        v_r_str.insert({std::to_string(i),gen_random_str(20)});
-    }
-//    l_prepare_data();
+    l_prepare_data();
     std::cout << "prepared " << v_r_str.size() << " count" << std::endl;
     auto end = std::chrono::steady_clock::now();
     std::cout << "Write Time: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000'000.0 << " sec" << std::endl;
 
+//    auto l_write_data = [&]() {
+//        auto block_count = count;
+//        std::vector<std::future<void>> v_futures;
+//        /// create several transactions
+//        auto conc_threads = std::thread::hardware_concurrency();
+//        std::cout << "Real thread count: " << conc_threads << std::endl;
+//        auto interval = block_count / conc_threads;
+//        for (size_t ith = 0; ith < conc_threads; ++ith)
+//        {
+//            v_futures.emplace_back(std::move(std::async(std::launch::async, [=, &v_r_str]()
+//            {
+//                size_t i = ith * interval;
+//                auto max_count = i + interval;
+//                std::cout << "Iter threads: " << ith << " Interval start: " << i << " Interval end: " << max_count << std::endl;
+//                for (; i < max_count; i++)
+//                {
+//                    Status s = db->Put(WriteOptions(), std::to_string(i), v_r_str[i]);
+//                    assert(s.ok());
+//                }
+//            })));
+//        }
+//        std::cout << "Wait futures" << std::endl;
+//        for (auto &it_future : v_futures)
+//        {
+//            it_future.get();
+//        }
 //
+//    };
 //    std::cout << "Write to db: " << count << " values." << std::endl;
-////    auto hash_val = std::hash<std::string>{}(random_value);
+//
 //    start = std::chrono::steady_clock::now();
-//    for (auto& it:v_r_str) {
-//        // Put key-value
-//        s = db->Put(WriteOptions(), it.first, it.second);
-//        assert(s.ok());
-//    }
+//    l_write_data();
 //    end = std::chrono::steady_clock::now();
 //    std::cout << "Write Time: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000'000.0 << " sec" << std::endl;
-//
-//
+
+
+    auto l_write_batch_data = [&]() {
+        auto block_count = count;
+        std::vector<std::future<void>> v_futures;
+        /// create several transactions
+        auto conc_threads = std::thread::hardware_concurrency();
+        conc_threads += 8;
+        std::cout << "Real thread count: " << conc_threads << std::endl;
+        auto interval = block_count / conc_threads;
+        for (size_t ith = 0; ith < conc_threads; ++ith)
+        {
+            v_futures.emplace_back(std::move(std::async(std::launch::async, [=, &v_r_str, &atom_count]()
+            {
+                size_t i = ith * interval;
+                auto max_count = i + interval;
+                std::cout << "Iter threads: " << ith << " Interval start: " << i << " Interval end: " << max_count << std::endl;
+
+                WriteBatch batch;
+                Status s;
+                auto it_count = 0;
+                auto batch_size = 10000;
+                auto commont_count = 0;
+
+                for (; i < max_count; i++)
+                {
+                    ++commont_count;
+                    if (it_count++ < batch_size)
+                    {
+                        batch.Put(std::to_string(i), v_r_str[i]);
+                    }
+                    else
+                    {
+                        s = db->Write(WriteOptions(), &batch);
+                        assert(s.ok());
+                        batch.Clear();
+                        it_count = 0;
+                    }
+                }
+                if (batch.Count())
+                {
+                    s = db->Write(WriteOptions(), &batch);
+                    assert(s.ok());
+                    batch.Clear();
+                }
+
+//                    Status s = db->Put(WriteOptions(), std::to_string(i), v_r_str[i]);
+//                    assert(s.ok());
+                atom_count += commont_count;
+                std::cout << "Common count " << commont_count << std::endl;
+            })));
+        }
+        std::cout << "Wait futures" << std::endl;
+        for (auto &it_future : v_futures)
+        {
+            it_future.get();
+        }
+
+    };
+    std::cout << "Write to db: " << count << " values." << std::endl;
+
+    start = std::chrono::steady_clock::now();
+    l_write_batch_data();
+    end = std::chrono::steady_clock::now();
+
+    std::cout << "Common count " << atom_count << std::endl;
+    std::cout << "Write Time: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000'000.0 << " sec" << std::endl;
+
 //    std::string value;
 //    start = std::chrono::steady_clock::now();
 //    for (auto& it:v_r_str) {
